d2716f7322ee60848bea9b736618726001425065
Caden H <caden.harper22@gmail.com>
2022-04-25 20:56:21 -0500

    started cart, menu-item, user components and added menu-item template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    MenuItemComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/menu-item.ts
@@ -0,0 +1,6 @@
+export interface MenuItem {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+}
