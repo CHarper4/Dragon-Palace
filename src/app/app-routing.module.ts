0650905c3a28ad64034b47c734ed846462f13e94
unknown <miraryan21@gmail.com>
2022-04-26 19:15:57 -0500

    bootstrap install, added home, menu and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+//import {RegisterComponent}
+//import {LoginComponent}
+import {CartComponent} from './cart/cart.component'
+import {UserComponent} from './user/user.component'
+
+// import component and add path below
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {path: 'menu', component: MenuComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'user', component: UserComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    [RouterModule.forRoot(routes)]
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
 import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     MenuItemComponent,
-    CartComponent
+    CartComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
